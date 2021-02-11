eea53c1a25747ec3faa402797db11f99e090542b
zhangc <411500976@qq.com>
2021-02-11 11:58:37 +0800

    fix

--- index.js
+++ index.js
@@ -9,37 +9,57 @@ var mouseConstructor = tryRequire('osx-mouse') || tryRequire('win-mouse')
 var supported = !!mouseConstructor
 var noop = function () { return noop }
 
-var drag = function (element) {
-  element = $(element)
+var drag = function(element) {
+	element = $(element);
 
-  var offset = null
-  var mouse = mouseConstructor()
+	var offset = null;
+	var win = null;
+	var size = null;
+	var mouse = mouseConstructor();
+  var isResizable
+	var onmousedown = function(e) {
+		win = remote.getCurrentWindow();
+		offset = [e.clientX, e.clientY];
+		size = win.getSize();
+    isResizable = win.isResizable()
+    if (isResizable) {
+      win.setResizable(false)
+    }
+	};
+  // var onmouseup =  function(e) {
+	// 	win.setResizable(true)
+	// };
 
-  var onmousedown = function (e) {
-    offset = [e.clientX, e.clientY]
-  }
+	element.on('mousedown', onmousedown);
+  // element.on('mouseup', onmouseup);
 
-  element.on('mousedown', onmousedown)
+	mouse.on('left-drag', function(x, y) {
+		if(!offset) return;
 
-  mouse.on('left-drag', function (x, y) {
-    if (!offset) return
+		var screenScale = remote.screen.getDisplayNearestPoint({ x, y }).scaleFactor;
+		x = Math.round(x / screenScale - offset[0]);
+		y = Math.round(y / screenScale - offset[1]);
+		win.setBounds({
+			width: size[0],
+			height: size[1],
+			x, y
+		});
+	});
 
-    x = Math.round(x - offset[0])
-    y = Math.round(y - offset[1])
+	mouse.on('left-up', function() {
+    if (win && isResizable) {
+      win.setResizable(true)
+    }
+		offset = null;
+		win = null;
+		size = null;
+	});
 
-    // setPosition throws error if called with -0
-    remote.getCurrentWindow().setPosition(x + 0, y + 0)
-  })
-
-  mouse.on('left-up', function () {
-    offset = null
-  })
-
-  return function () {
-    element.off('mousedown', onmousedown)
-    mouse.destroy()
-  }
-}
+	return function() {
+		element.off('mousedown', onmousedown);
+		mouse.destroy();
+	};
+};
 
 drag.supported = supported
 module.exports = supported ? drag : noop
